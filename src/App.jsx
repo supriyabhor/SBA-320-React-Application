ac950c57a90a6ff3880bdc487435c42c260b241b
supriyabhor <supriyanaikwadi89@gmail.com>
2025-02-17 14:32:26 -0500

    Fetch the dada

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState , useEffect } from 'react'
-import ArtGallery from './components/ArtGallery';
-import ArtPieceDetails from './components/ArtPieceDetails';
+// import ArtGallery from './components/ArtGallery';
+// import ArtPieceDetails from './components/ArtPieceDetails';
 import Form from './components/Form';
 
 import './App.css'
@@ -14,7 +14,13 @@ function App() {
   const API_URL = `https://api.artic.edu/api/v1/artworks?fields=id,title,artist_title,thumbnail&limit=100&key=${API_Key}`;
 
   useEffect(() => {
-    
+    const fetchArtPieces = async () => {
+      const res = await fetch(API_URL);
+      const jsoaData = await res.json();
+      console.log(jsoaData);
+      setArtPieces(jsoaData.data);
+    };
+    fetchArtPieces();
   },[]);
 
   return (
