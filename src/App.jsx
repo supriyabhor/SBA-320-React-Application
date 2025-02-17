d8fb050d6d084cced002a28098be226ccb95e9ee
supriyabhor <supriyanaikwadi89@gmail.com>
2025-02-17 14:01:45 -0500

    initial comment

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState , useEffect } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Art Gallery of Chicago</h1>
+    </>
+  )
+}
+
+export default App
